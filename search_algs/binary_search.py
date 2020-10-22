b5063312984bc4e0dfdbd4cbab4d2feb5ef970a0
MLX Jordina Gavalda <jordinagavalda@gmail.com>
2020-10-22 20:29:56 +0200

    modified

--- search_algs/binary_search.py
+++ search_algs/binary_search.py
@@ -1,17 +1,14 @@
 def binary(data, n):
     if len(data) == 0 or (len(data) == 1 and data[0] != n):
-        print('not here')
+        return False
 
-    try:
-        mid = data[len(data) // 2]
-    except:
-        print('Error, try again')
-        quit()
+    mid = data[len(data) // 2]
 
-    if mid == n: print('Found it', n)
+
+    if mid == n: return True
     if mid > n: return binary(data[:len(data) // 2], n)
     if mid < n: return binary(data[len(data) // 2 + 1:], n)
 
 
-data = [-4, -2, -1, 1, 2, 3.5, 4, 5, 6, 7]
-binary(data, 8)
+data = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+print(binary(data, 4))
